a9aa8da9fab4abdcbb8f0c136a5621f64075488d
eadiyatma <efran.adiyatma9g@gmail.com>
2022-07-07 11:41:33 +0700

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup>
+import DashboardComponent from "../components/DashboardComponent.vue";
+// import TheWelcome from '@/components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <DashboardComponent />
+  </main>
+</template>
