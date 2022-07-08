f9ea01fd990df5fd7f466b6feb55aaab1afcc136
eadiyatma <efran.adiyatma9g@gmail.com>
2022-07-08 10:11:13 +0700

    fix sidebar

--- src/components/SidebarComponent.vue
+++ src/components/SidebarComponent.vue
@@ -1,5 +1,23 @@
+<script setup>
+import { useRouter } from "vue-router";
+import IconSegment from "./icons/IconSegment.vue";
+const iconDashboard = new URL(`../assets/iconDashboard.svg`, import.meta.url)
+  .href;
+
+const router = useRouter();
+
+console.log(router.currentRoute);
+</script>
+
 <template>
-  <nav class="h-screen border w-[72px] fixed bg-white pt-[70px]">
-    ini side nav
+  <nav class="h-screen border w-[72px] fixed bg-white pt-[70px] flex flex-col">
+    <div class="p-4 py-2 -scale-x-100">
+      <IconSegment />
+    </div>
+    <div class="flex p-4 items-center justify-center bg-gray-300">
+      <button class="flex bg-white w-6 p-1 rounded">
+        <img class="mx-auto" :src="iconDashboard" />
+      </button>
+    </div>
   </nav>
 </template>
